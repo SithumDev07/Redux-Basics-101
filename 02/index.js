800a3997b86c470bb295dfdbf2acafe91de1cf34
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-12 22:59:41 +0530

    Installed React Redux and React-Dom

---
+++ 02/index.js
@@ -0,0 +1,84 @@
+import { createStore } from "redux";
+
+
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
+
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
+//Never mutate State
+
+const reducer = (state = [], action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return [{text: action.text, id: Date.now()},...state ];
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe( () => {
+  console.log(store.getState());
+} );
+
+const dispatchAddTodo = text => {
+  store.dispatch(addTodo(text));
+}
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+
+const paintTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = '';
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'Del';
+    btn.addEventListener('click', dispatchDeleteTodo)
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintTodos);
+
+
+// const createTodo = todo => {
+//   const li = document.createElement('li');
+//   li.innerText = todo;
+//   ul.appendChild(li);
+// }
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  dispatchAddTodo(toDo);
+}
+
+form.addEventListener('submit', onSubmit);

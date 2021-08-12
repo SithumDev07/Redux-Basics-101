77a30b842ad2cffe8e2de54f7f6fd0dbc28ed5e1
SithumDev07 <sithum.dashantha@gmail.com>
2021-08-12 06:14:34 +0530

    Adding and deleting todos using redux store

--- src/index.js
+++ src/index.js
@@ -8,14 +8,28 @@ const ul = document.querySelector('ul');
 const ADD_TODO = 'ADD_TODO';
 const DELETE_TODO = 'DELETE_TODO';
 
+const addTodo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  }
+}
+
+const deleteTodo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+}
+
 //Never mutate State
 
 const reducer = (state = [], action) => {
   switch(action.type) {
     case ADD_TODO:
-      return [...state, {text: action.text}];
+      return [{text: action.text, id: Date.now()},...state ];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -27,6 +41,33 @@ store.subscribe( () => {
   console.log(store.getState());
 } );
 
+const dispatchAddTodo = text => {
+  store.dispatch(addTodo(text));
+}
+
+const dispatchDeleteTodo = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+}
+
+const paintTodos = () => {
+  const todos = store.getState();
+  ul.innerHTML = '';
+  todos.forEach(todo => {
+    const li = document.createElement('li');
+    const btn = document.createElement('button');
+    btn.innerText = 'Del';
+    btn.addEventListener('click', dispatchDeleteTodo)
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  })
+}
+
+store.subscribe(paintTodos);
+
+
 // const createTodo = todo => {
 //   const li = document.createElement('li');
 //   li.innerText = todo;
@@ -37,7 +78,7 @@ const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddTodo(toDo);
 }
 
 form.addEventListener('submit', onSubmit);
